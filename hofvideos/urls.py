085c634f22e2aa257897df08e61d8fa6b5c1334d
Harsh Vartak <vartak.harsh@gmail.com>
2019-09-25 01:03:25 +0530

    Addition of models and a little progress

--- hofvideos/urls.py
+++ hofvideos/urls.py
@@ -25,7 +25,15 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
+    #Authentication
     path('signup', views.SignUp.as_view(),  name="signup"),
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    #halls
+    path('halloffame/create', views.CreateHall.as_view(),name='create_hall'),
+
+
+
+
+
 ]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
