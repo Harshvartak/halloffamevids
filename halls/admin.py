09744121950ac618ed2bc6f85dabb526f150e235
Harsh Vartak <vartak.harsh@gmail.com>
2019-09-25 09:46:24 +0530

    Addition and display of hall model

--- halls/admin.py
+++ halls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from.models import Hall, Video
+from .models import Hall, Video
 
 admin.site.register(Hall)
 admin.site.register(Video)
--- hofvideos/settings.py
+++ hofvideos/settings.py
@@ -83,14 +83,11 @@ WSGI_APPLICATION = 'hofvideos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':  'halldb',
-        'USER': 'postgres',
-        'PASSWORD':'harshv18',
-        'HOST':'localhost',
-        'PORT':'5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
